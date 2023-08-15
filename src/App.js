743ccd95ed4f74e8a4d5727a484ca97283577503
saidulixlam <saidulixlam@gmail.com>
2023-08-15 10:45:18 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Fragment, useState } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge ,uCollege) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() ,college: uCollege},
+      ];
+    });
+  };
+
+  return (
+    <Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </Fragment>
+  );
+}
+
+export default App;
